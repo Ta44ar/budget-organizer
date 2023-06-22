906fc31b64d4c85fcc3bf8598db350b04faf3a2c
misiegn <niemich99@gmail.com>
2023-06-22 21:01:02 +0200

    organizacja v2

--- home_budget/models.py
+++ home_budget/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
-class wydatek(models.Model):
+class transakcja(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nazwa = models.CharField(max_length=50)
     kategoria = models.CharField(max_length=50)
     kwota = models.FloatField()
     data_powstania = models.DateTimeField(default=timezone.now)
 
-class przychod(models.Model):
-    nazwa = models.CharField(max_length=50)
-    kategoria = models.CharField(max_length=50)
-    kwota = models.FloatField()
-    data_powstania = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        abstract = True
+        verbose_name = "transakcja"
+        verbose_name_plural = "transakcje"
+
+class wydatek(transakcja):
+    pass
+
+class przychod(transakcja):
+    pass
--- home_budget/views.py
+++ home_budget/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import wydatek, przychod
 
-# Create your views here.
+def lista_wydatkow(request):
+    wydatki = wydatek.objects.all()
+    return render(request, 'home_budget/lista_wydatkow.html', {'wydatki': wydatki})
+
+def lista_przychodow(request):
+    przychody = przychod.objects.all()
+    return render(request, 'home_budget/lista_przychodow.html', {'przychody': przychody})
