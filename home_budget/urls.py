8540686d201495321d3b5bc4027d1d8be0326e98
misiegn <niemich99@gmail.com>
2023-06-23 16:28:09 +0200

    zestawienie v1

--- home_budget/urls.py
+++ home_budget/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('wydatki/', views.lista_wydatkow, name='lista_wydatkow'),
     path('przychody/', views.lista_przychodow, name='lista_przychodow'),
+    path('dodaj_wydatek/', views.dodaj_wydatek, name='dodaj_wydatek'),
+    path('dodaj_przychod/', views.dodaj_przychod, name='dodaj_przychod'),
+    path('zestawienie/', views.zestawienie, name='zestawienie'),
 ]
+
     
--- home_budget/views.py
+++ home_budget/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Wydatek, Przychod
+from .forms import WydatekForm, PrzychodForm
 
 def home(request):
     return render(request, 'home_budget/base.html')
@@ -11,3 +13,34 @@ def lista_wydatkow(request):
 def lista_przychodow(request):
     przychody = Przychod.objects.order_by('data_powstania')
     return render(request, 'home_budget/lista_przychodow.html', {'przychody': przychody})
+
+def dodaj_wydatek(request):
+    if request.method == "POST":
+        form = WydatekForm(request.POST)
+        if form.is_valid():
+            wydatek = form.save(commit=False)
+            wydatek.autor = request.user
+            wydatek.data_powstania = timezone.now()
+            wydatek.save()
+            return redirect('lista_wydatkow')
+    else:
+        form = WydatekForm()
+    return render(request, 'home_budget/dodaj_wydatek.html', {'form': form})
+
+def dodaj_przychod(request):
+    if request.method == "POST":
+        form = PrzychodForm(request.POST)
+        if form.is_valid():
+            przychod = form.save(commit=False)
+            przychod.autor = request.user
+            przychod.data_powstania = timezone.now()
+            przychod.save()
+            return redirect('lista_przychodow')
+    else:
+        form = PrzychodForm()
+    return render(request, 'home_budget/dodaj_przychod.html', {'form': form})
+
+def zestawienie(request):
+    wydatki = Wydatek.objects.all()
+    przychody = Przychod.objects.all()
+    return render(request, 'home_budget/zestawienie.html', {'wydatki': wydatki, 'przychody': przychody})
